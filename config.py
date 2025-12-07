2deb1e0478de793211c7e8895db85daa19e5da2a
AsfanUlla <asfanulla@gmail.com>
2025-12-07 21:30:59 +0530

    config

---
+++ config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+DATA_FOLDER = Path('data')
+CACHE_FOLDER = DATA_FOLDER.joinpath('cache')
+TICKER_LIST_FOLDER = DATA_FOLDER.joinpath('ticker_list')
+
+#NASDAQ screener api
+NASDAQ_SCREENER_API = 'https://api.nasdaq.com/api/screener/stocks?exchange={}&download=true'
+
+#API to download listed tickers on NYSE, NASDAQ, AMEX
+TICKER_LIST_URLS = dict(
+    nyse=NASDAQ_SCREENER_API.format('NYSE'),
+    nasdaq=NASDAQ_SCREENER_API.format('NASDAQ'),
+    amex=NASDAQ_SCREENER_API.format('AMEX'),
+)
